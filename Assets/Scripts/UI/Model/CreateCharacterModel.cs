67be07179f375641472fbc3bf70ad0e7850ff193
FSH943503596 <38723993+FSH943503596@users.noreply.github.com>
2018-09-01 21:12:06 +0800

    定义数据类Modle

---
+++ Assets/Scripts/UI/Model/CreateCharacterModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Model
+{
+    public class CreateCharacterModel
+    {
+        public struct CreateCharacterViewModel
+        {
+            public uint raceID { get; set; }
+            public string raceName { get; set; }
+            public string raceDes { get; set; }
+            public string initValue { get; set; }
+            public string growthValue { get; set; }
+            public string raceAbilityOne { get; set; }
+            public string raceAbilityTwo { get; set; }
+        }
+
+        private List<CreateCharacterViewModel> m_createCharacterViewModels;
+
+        public List<CreateCharacterViewModel> createCharacterViewModels
+        {
+            get
+            {
+                return m_createCharacterViewModels;
+            }
+
+            set
+            {
+                m_createCharacterViewModels = value;
+            }
+        }
+
+    }
+    
+
+}
--- Assets/Scripts/UI/Model/RocaDescribeModel.cs
+++ Assets/Scripts/UI/Model/RocaDescribeModel.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateCharacterPanel : MonoBehaviour
+namespace UI.Model
 {
+    public class RocaDescribeModel
+    {
 
+
+    }
 
 }
