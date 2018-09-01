e0d5ebcef6cd6f36f568eee436aae1132c8bef26
seaflying <seaflying@live.cn>
2018-09-01 20:04:30 +0800

    增加初始场景， 工具类
    Mono单例， class单例， 开始场景

---
+++ Assets/Scripts/Common/GameStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    //游戏启动逻辑
+
+
+	}	
+}
