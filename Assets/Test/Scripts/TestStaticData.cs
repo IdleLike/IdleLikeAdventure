336b0d0d93ab331be92b31b5a9d04d239ba080d0
seaflying <seaflying@live.cn>
2018-09-01 16:34:41 +0800

    Create
    Co-Authored-By: seaflying <seaflying@users.noreply.github.com>

---
+++ Assets/Test/Scripts/TestStaticData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable][CreateAssetMenu]
+public class TestStaticData : ScriptableObject {
+
+    private static TestStaticData instance;
+    public static TestStaticData Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<TestStaticData>("TestData");
+            }
+            return instance;
+        }
+    }
+
+
+
+
+
+}
