9e12427e4e7d355628ec77a627e0c8e33d9ea9b1
seaflying <seaflying@live.cn>
2018-09-09 17:34:13 +0800

    创建角色接口

--- Assets/Scripts/Services/ActorService.cs
+++ Assets/Scripts/Services/ActorService.cs
@@ -6,8 +6,16 @@ namespace Service
 {
     public class ActorService : BaseService
     {
-
-
+        /// <summary>
+        /// 创建英雄
+        /// </summary>
+        /// <param name="userID">User identifier.</param>
+        /// <param name="heroConfigID">Hero config identifier.</param>
+        /// <param name="heroName">Hero name.</param>
+        public void CreateHero(int userID, uint heroConfigID, string heroName)
+        {
+            
+        }
     }
 }
 
