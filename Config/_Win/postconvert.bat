95ee3bd8a0559e4563c03d9f9632268597000f9a
seaflying88 <seaflying@live.cn>
2018-09-09 14:16:16 +0800

    修改命令行

--- Config/_Win/postconvert.bat
+++ Config/_Win/postconvert.bat
@@ -1,13 +1,13 @@
 echo off & color 0A
-::指定起始文件夹
+
 cd..
 set DIR=%CD%\Output\Bin_C
 echo DIR=%DIR%
-set MGRDIR = %CD%\Output\Code_C\StaticDataMgr.cs
+set MGRDIR=%CD%\Output\Code_C\StaticDataMgr.cs
 echo DIR=%MGRDIR%
-Set CODEDIR = %CD%\Output\Code_C\Data
+Set CODEDIR=%CD%\Output\Code_C\Data
 echo CODEDIR=%CODEDIR%
-::指定起始文件夹 Byte文件目标文件夹
+
 cd..
 set ToDIR=%CD%\Assets\Resources\Configs\Bytes
 echo ToDIR=%ToDIR%
